0edc79459ca5013414e5d6877423c7f7f7cd6f15
Steve Garton <sheepeatingtaz@gmail.com>
2021-04-05 11:33:10 +0100

    Template tweaks

--- operation/settings.py
+++ operation/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vx@08(h7!9-w%xx0lyjqlqs%dhw5zr$s(_xqj==)jw()$a1k4&'
+SECRET_KEY = os.getenv('SECRET_KEY', "Dummy")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
